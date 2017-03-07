dd12c4537e29c35c2f8d538b110acca8890741d6
menachem <menachem238@users.noreply.github.com>
2017-03-06 23:05:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo ('hello world');
+echo ('this will be the testing ground for my site because I can uoload and edit from github. but I am not sure why I need php support.');
 ?>
+ meh. it will be useful at some point.
