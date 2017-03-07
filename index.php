35429304e2ba50e0cfddebefa0a0d47ce3b7ecdb
menachem <menachem238@users.noreply.github.com>
2017-03-07 07:46:58 -0500

    Update index to have same code as current site

--- index.php
+++ index.php
@@ -1,2 +1,148 @@
-this will be the testing ground for my site because I can upload and edit from github. but I am not sure why I need php support. 
-meh. it will be useful at some point.
+<!-- this will be the testing ground for my site because I can upload and edit from github. but I am not sure why I need php support. 
+meh. it will be useful at some point. -->
+
+
+<!doctype html>
+<html>
+<head>
+  <title>menachem | menachem238</title>
+	<link rel="shortcut icon" type="image/png" href="/icon.png">
+	
+  <meta charset='utf-8' />
+  <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0' />
+  <meta name="description" content="the web site of menachem aka menachem238 / maauer" />
+	<meta name='keywords' content='menachem,menachem238,maauer'/>
+  <meta name="author" content="menachem" />
+  <meta name="author" content="menachem238" />
+	<meta name="google" content="notranslate" />
+
+  <!-- opengraph -->
+  <meta property="og:url" content="https://maauer.com" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="menachem | menachem238" />
+  <meta property="og:description" content="the web site of menachem aka menachem238 / maauer" />
+
+
+  <style>
+    /* html and body */
+    html, body {margin: 0; padding: 0; background-color:#181818; }
+    
+    /* begin title and links section (unorganized) */
+    p.title {display: inline; color: #ffffff; font-size:6em; font-family: 'Roboto Slab', sans-serif, monospace; font-weight:lighter;}
+    div.links {display: block; color: #ffffff; font-size:16px; font-weight:900; font-family: 'Roboto Slab', sans-serif, monospace;  font-weight:normal;}
+    div.title {position:absolute; left:15%; top:35%;}
+    /* links */
+    div.links a:hover {position:relative; bottom: 0.1em;}
+
+    a:link, a:visited {text-decoration: none; color: #bf0f0f;}
+    p.title:hover ~ #add-238 {color: #ffffff; opacity: 0.45;}
+    
+    /* animations for the "238" */
+    #add-238 {color: #181818; opacity:0;}
+    #add-238:hover {color: #ffffff; opacity:0.45;}
+    #add-238 {
+      transition: all 0.50s;
+      -webkit-transition: all 0.50s;
+      -moz-transition: all 0.50s;
+      -o-transition: all 0.50s;}
+    #add-238:hover {
+      transition: all 2.0s;
+      -webkit-transition: all 2.0s;
+      -moz-transition: all 2.0s;
+      -o-transition: all 2.0s;}
+
+    /* end title */
+    
+    
+		/* below0 - absolute container for relitave objects that lives below 100%*/
+		div.below0 {
+			position:absolute;
+			top:105%;
+			left:0;
+			right:0;
+			width: 100%; /* may need to change with quote */
+			height: auto;
+		}
+		
+			/* quote of the day - quoteotd */
+      	/* div positioning & size */ 
+				div.quoteotd {
+        	position:relative;
+        	width: auto; /* may need to change with quote */
+        	height: auto;
+      	}
+      
+      	/* styling */
+      	p.quoteotd {
+        	padding-right:6%;
+        	padding-left:6%;
+        	color:#ffffff;
+        	font-family: 'Roboto Slab', sans-serif, monospace;
+        	font-size: 1.3rem;
+					text-align: center;
+      	}
+    	/* end quote of the day */
+    /* end below0 */
+    
+    /* hr - yes I know it is html5 but blah blah... */
+    .hr { 
+    display: block;
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
+    margin-left: auto;
+    margin-right: auto;
+    border-style: solid;
+    border-width: 1px;
+    color:#ffffff;
+		right:5%;
+    left:5%;
+    position:absolute;
+    } 
+    
+    
+    /* media queries for smaller screens */
+    @media (max-width: 1000px){
+      /* title */
+      p.title {font-size:10vw;}
+    }
+    @media (max-width: 500px){
+      /* title */
+      p.title {font-size:12vw;}
+    }
+    @media (max-width: 350px){
+      /* title */
+      p.title {font-size:10vw;}
+      a.links {font-size:10px;}
+    }
+    
+  </style>
+</head>
+<body>
+  <div class='title'>
+    <p class='title'>men&#10701;chem</p><p class='title' id='add-238'>238</p>
+    <div class='links'>
+      <a class='links' href='/github' target='_blank'>GITHUB</a>&emsp;
+      <a class='links' href='/projects/'>PROJECTS</a>&emsp;
+      <!-- <a class='links' href='/twitter' target='_blank'>TWITTER</a>&emsp; -->
+      <a class='links' href='/codepen' target='_blank'>CODEPEN</a>&emsp;
+    </div> <!-- close 'links' -->
+  </div> <!-- close 'title' -->
+
+  <span class='hr' style='top:103%;'></span>
+  
+	<div class='below0'>
+    
+  	<div class='quoteotd'>
+    	<p class='quoteotd'>Time goes by, people change, paint fades, buildings get torn down. But through, through the shivering and changing and the __________ look of consistancy in you life, your memories will live on untainted. For they are pure and perfect. Rain will not erode them, bulldozers cannot level them and the actions of others will never corrupt them.
+      </p>
+      <p class='quoteotd'>--S1 '09 - '10  -xoxoxox</p>
+  	</div>
+  </div>
+  
+<!-- end -->
+<link href="/fonts/roboto-slab.css" rel="stylesheet">
+
+</body>
+</html>
+
+<!-- copyright maauer.com 2016-2017 - but you can do whatever you want with it. If you do please give me credit by linking back here. in a comment or "a" tag-->
